1940769ba1435a4637941ae8fa0c0d6de186df59
AlexWhite2017 <tmpromspb@gmail.com>
2026-03-23 20:39:01 +0300

    Add files via upload

---
+++ bash_20260323_b288a9.sh
@@ -0,0 +1,10 @@
+# Создайте виртуальное окружение (рекомендуется)
+python3 -m venv venv
+source venv/bin/activate  # Для Linux/Mac
+# venv\Scripts\activate   # Для Windows
+
+# Установите зависимости
+pip install -r requirements.txt
+
+# Запустите бота
+python bot.py
